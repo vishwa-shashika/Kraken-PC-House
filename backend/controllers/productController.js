a17d639fca69295c3b2f4661f51484c801bdebe5
Vishwa Amarasinghe <vishwa.amarasinghe4@gmail.com>
2022-06-20 22:15:31 +0530

    Get Single Product Route Added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,16 +3,22 @@ const Product = require("../models/product");
 //Create New Product => api/v1/new
 exports.newProduct = async (req, res, next) => {
   const product = await Product.create(req.body);
-
-  res.status(201).json({
-    success: true,
-    product,
-  });
+  res.status(201).json({ success: true, product });
 };
 
 //Get All Products => api/v1/products
 exports.getProducts = async (req, res, next) => {
   const products = await Product.find();
-  console.log(products);
   res.status(200).json({ success: true, count: products.length, products });
+};
+
+//Get Single Product Detail => api/v1/products/:id
+exports.getSingleProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Product not found" });
+  }
+  res.status(200).json({ success: true, product });
 };
