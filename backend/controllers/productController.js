acf621bc5471a755edf47a7d5cb86154213700a7
Vishwa Amarasinghe <vishwa.amarasinghe4@gmail.com>
2022-06-21 08:54:16 +0530

    Update Product Route Added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -22,3 +22,19 @@ exports.getSingleProduct = async (req, res, next) => {
   }
   res.status(200).json({ success: true, product });
 };
+
+//Update Product => api/v1/products/:id
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+  if (!product) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Product not found" });
+  }
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  res.status(200).json({ success: true, product });
+};
