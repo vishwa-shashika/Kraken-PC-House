88e6b240c1c38bcdf4a1ba8d4d81444e36915b66
Vishwa Amarasinghe <vishwa.amarasinghe4@gmail.com>
2022-06-22 22:52:23 +0530

    Filtering Added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,10 +5,11 @@ const APIFeatures = require("../utils/apiFeatures");
 
 //Get All Products => api/v1/products===================================================================================
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
-  const apiFeatures = new APIFeatures(Product.find(), req.query).search();
+  const apiFeatures = new APIFeatures(Product.find(), req.query)
+    .search()
+    .filter();
 
-  const products = await apiFeatures.quary;
-  //const products = await Product.find();
+  const products = await apiFeatures.query;
 
   res.status(200).json({
     success: true,
--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -1,21 +1,39 @@
+const { query } = require("express");
+
 class APIFeatures {
-  constructor(quary, quaryStr) {
-    this.quary = quary;
-    this.quaryStr = quaryStr;
+  constructor(query, queryStr) {
+    this.query = query;
+    this.queryStr = queryStr;
   }
 
+  //Search Fuction======================================================================================================
   search() {
-    const keyword = this.quaryStr.keyword
+    const keyword = this.queryStr.keyword
       ? {
           name: {
-            $regex: this.quaryStr.keyword,
+            $regex: this.queryStr.keyword,
             $options: "i",
           },
         }
       : {};
 
-    console.log(keyword);
-    this.quary = this.quary.find({ ...keyword });
+    this.query = this.query.find({ ...keyword });
+    return this;
+  }
+
+  //Filter Function=====================================================================================================
+  filter() {
+    const queryCopy = { ...this.queryStr };
+
+    //Removing Fields from the query
+    const removeFields = ["keyword", "limit", "page"];
+    removeFields.forEach((el) => delete queryCopy[el]);
+
+    //Advanced Filter for price
+    let queryStr = JSON.stringify(queryCopy);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
+
+    this.query = this.query.find(JSON.parse(queryStr));
     return this;
   }
 }
