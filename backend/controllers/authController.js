17b227824051f6741a8b034d4d1a5dfcccf0ea8d
Vishwa Amarasinghe <vishwa.amarasinghe4@gmail.com>
2022-06-24 03:35:54 +0530

    Get User Profile Route Added

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -112,6 +112,15 @@ exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
   sendToken(user, 200, res);
 });
 
+//Get Currently Logged In User Details  => /api/v1/me
+exports.getUserProfile = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
+
 //Logout User     => /api/v1/logout=====================================================================================
 exports.logout = catchAsyncErrors(async (req, res, next) => {
   res.cookie("token", null, {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,12 +7,17 @@ const {
   logout,
   forgotPassword,
   resetPassword,
+  getUserProfile,
 } = require("../controllers/authController");
+
+const { isAuthenticatedUser } = require("../middlewares/auth");
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 router.route("/logout").get(logout);
+
+router.route("/me").get(isAuthenticatedUser, getUserProfile);
 
 module.exports = router;
