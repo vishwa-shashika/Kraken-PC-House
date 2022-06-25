1c3607bdb5f22b1f761071e584b6bfef2c7beef6
Vishwa Amarasinghe <vishwa.amarasinghe4@gmail.com>
2022-06-26 02:04:01 +0530

    Tempory Header and Footer Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import Footer from "./components/layouts/Footer";
+import Header from "./components/layouts/Header";
 
 function App() {
   return (
     <div>
-      <h1 className="text-4xl">Hello Kraken PC House</h1>
+      <Header />
+      <h1 className="text-4xl text-center h-[100vh]">Hello Kraken PC House</h1>
+      <Footer />
     </div>
   );
 }
